d5f59f747e8cd6434edc2870937690fc929529df
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2014-08-27 00:04:54 +0800

    configure travis-ci

--- ext/pocket_sphinx/extconf.rb
+++ ext/pocket_sphinx/extconf.rb
@@ -1,10 +1,16 @@
 require 'mkmf'
 
-unless find_header 'pocketsphinx_export.h', '/usr/local/include/pocketsphinx'
+unless find_header 'pocketsphinx_export.h', '/usr/local/include/pocketsphinx', '/usr/include/pocketsphinx'
   abort 'cannot find pocketsphinx, please install it.'
 end
-find_header 'cmd_ln.h', '/usr/local/include/sphinxbase'
-find_library 'pocketsphinx', 'ps_init'
-find_library 'sphinxbase', 'cmd_ln_init'
+unless find_header 'cmd_ln.h', '/usr/local/include/sphinxbase', '/usr/include/sphinxbase'
+  abort 'cannot find sphinxbase, please install it.'
+end
+unless find_library 'pocketsphinx', 'ps_init'
+  find_library 'libpocketsphinx', 'ps_init'
+end
+unless find_library 'sphinxbase', 'cmd_ln_init'
+  find_library 'libsphinxbase', 'cmd_ln_init'
+end
 
 create_makefile 'pocket_sphinx/pocket_sphinx'
